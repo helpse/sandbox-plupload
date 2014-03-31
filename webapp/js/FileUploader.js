9a9330d36ad5ca2a7cbed51be209605c58ec5192
Dustin Getz <dgetz@wingspan.com>
2014-03-31 17:58:29 -0400

    wip

--- webapp/js/FileUploader.js
+++ webapp/js/FileUploader.js
@@ -7,7 +7,7 @@ define([
 
 
     var DEFAULT_SETTINGS = {
-        runtimes: 'html5',
+        runtimes: 'flash,html5',
         flash_swf_url: 'lib/plupload/plupload.flash.swf',
         multipart: true,
         multi_selection: true,
@@ -35,7 +35,7 @@ define([
                 <div>
                     <button id="42">Select file...</button>
                     <button onClick={this.onUploadButtonClick}>Upload</button>
-                    <pre>{JSON.stringify(this.state.fileQueue, undefined, 2)}</pre>
+                    <pre>{JSON.stringify(_.pluck(this.state.fileQueue, 'name'), undefined, 2)}</pre>
                 </div>
             );
         },
@@ -60,61 +60,60 @@ define([
 
         onFilesAdded: function (uploader, files) {
             this.setState({
-                fileQueue: _.union(this.state.fileQueue, _.pluck(files, 'name'))
+                fileQueue: _.union(this.state.fileQueue, [files])
             });
         },
 
         onFilesRemoved: function (uploader, files) {
             this.setState({
-                fileQueue: _.difference(this.state.fileQueue, _.pluck(files, 'name'))
+                fileQueue: _.difference(this.state.fileQueue, [files])
             });
         },
 
         onUploadButtonClick: function () {
             var settings = _.defaults({ url: this.props.url }, this.settings);
-            var doUpload = _.partial(uploadOneFile, settings);
+            var doUpload = _.partial(this.uploadOneFile, settings);
 
             asyncEachSerial(doUpload, this.uploader.files) // use the uploader's file reference directly, react state only mirrors it
                 .fin(function (results) {
                     console.log('fin ', results);
                 })
                 .done();
+        },
+
+        uploadOneFile: function (settings, fileRef) {
+            var uploader = this.uploader; //new plupload.Uploader(settings);
+
+            // Abuse private sate -
+            // Add the native file object directly into the plupload list of file objects.
+            uploader.files = [fileRef];
+
+            var deferred = Q.defer();
+
+            function onFileUploaded (uploader, file, response) {
+                uploader.unbind('FileUploaded', onFileUploaded);
+                uploader.unbind('UploadProgress', onUploadProgress);
+                uploader.unbind('Error', onError);
+                deferred.resolve(response);
+                uploader.destroy();
+            }
+
+            function onUploadProgress(uploader, file) {
+                deferred.notify(file);
+            }
+
+            function onError (uploader, err) {
+                deferred.reject(err);
+            }
+
+            uploader.bind('FileUploaded', onFileUploaded);
+            uploader.bind('UploadProgress', onUploadProgress);
+            uploader.bind('Error', onError);
+            //uploader.init();
+            setTimeout(_.bind(uploader.start, uploader), 100);
+            return deferred.promise;
         }
     });
-
-
-    function uploadOneFile (settings, fileRef) {
-        var uploader = new plupload.Uploader(settings);
-
-        // Abuse private sate -
-        // Add the native file object directly into the plupload list of file objects.
-        uploader.files.push(fileRef);
-
-        var deferred = Q.defer();
-
-        function onFileUploaded (uploader, file, response) {
-            uploader.unbind('FileUploaded', onFileUploaded);
-            uploader.unbind('UploadProgress', onUploadProgress);
-            uploader.unbind('Error', onError);
-            deferred.resolve(response);
-            uploader.destroy();
-        }
-
-        function onUploadProgress(uploader, file) {
-            deferred.notify(file);
-        }
-
-        function onError (uploader, err) {
-            deferred.reject(err);
-        }
-
-        uploader.bind('FileUploaded', onFileUploaded);
-        uploader.bind('UploadProgress', onUploadProgress);
-        uploader.bind('Error', onError);
-        uploader.init();
-        setTimeout(_.bind(uploader.start, uploader), 100);
-        return deferred.promise;
-    }
 
     /**
      * Iterates over a list, applying a function to each element.
