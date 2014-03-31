612ff3b0c781be1d887b0b357b66db8c7afd592e
Dustin Getz <dgetz@wingspan.com>
2014-03-31 16:39:39 -0400

    base code for serializing multiple upload requests, one req per file

--- webapp/js/FileUploader.js
+++ webapp/js/FileUploader.js
@@ -7,7 +7,7 @@ define([
 
 
     var DEFAULT_SETTINGS = {
-        runtimes: 'flash',
+        runtimes: 'html5',
         flash_swf_url: 'lib/plupload/plupload.flash.swf',
         multipart: true,
         multi_selection: true,
@@ -42,18 +42,20 @@ define([
 
         componentDidMount: function () {
 
-            var settings = _.defaults({
-                browse_button: '42',
-                url: this.props.url
+            this.settings = _.defaults({
+                browse_button: '42'
             }, DEFAULT_SETTINGS);
 
-            this.uploader = new plupload.Uploader(settings);
+            this.uploader = new plupload.Uploader(this.settings);
             this.uploader.bind('FilesAdded', this.onFilesAdded);
+            this.uploader.bind('FilesRemoved', this.onFilesRemoved);
             this.uploader.init();
         },
 
         componentWillUnmount: function () {
             this.uploader.unbind('FilesAdded', this.onFilesAdded);
+            this.uploader.unbind('FilesRemoved', this.onFilesRemoved);
+            this.uploader.destroy();
         },
 
         onFilesAdded: function (uploader, files) {
@@ -62,42 +64,72 @@ define([
             });
         },
 
-        onUploadButtonClick: function () {
-            this.upload()
-                .then(function () {
-                    console.log('success');
-                })
-                .fail(function () {
-                    console.log('fail');
-                })
-                .fin(function () {
-                    console.log('fin');
-                })
-                .done();
+        onFilesRemoved: function (uploader, files) {
+            this.setState({
+                fileQueue: _.difference(this.state.fileQueue, _.pluck(files, 'name'))
+            });
         },
 
-        upload: function () {
-            var deferred = Q.defer();
+        onUploadButtonClick: function () {
+            var settings = _.defaults({ url: this.props.url }, this.settings);
+            var doUpload = _.partial(uploadOneFile, settings);
 
-            function onFileUploaded (uploader, file, response) {
-                uploader.unbind('FileUploaded', onFileUploaded);
-                uploader.unbind('UploadProgress', onUploadProgress);
-                uploader.unbind('Error', onError);
-                deferred.resolve(response);
-            }
-
-            function onUploadProgress(uploader, file) {
-                deferred.notify(file);
-            }
-
-            function onError (uploader, err) {
-                deferred.reject(err);
-            }
-
-            this.uploader.start();
-            return deferred.promise;
+            asyncEachSerial(doUpload, this.uploader.files) // use the uploader's file reference directly, react state only mirrors it
+                .fin(function (results) {
+                    console.log('fin ', results);
+                })
+                .done();
         }
     });
+
+
+    function uploadOneFile (settings, fileRef) {
+        var uploader = new plupload.Uploader(settings);
+        uploader.files.push(fileRef); // Add the native file object directly into the plupload list of file objects.
+
+        var deferred = Q.defer();
+
+        function onFileUploaded (uploader, file, response) {
+            uploader.unbind('FileUploaded', onFileUploaded);
+            uploader.unbind('UploadProgress', onUploadProgress);
+            uploader.unbind('Error', onError);
+            deferred.resolve(response);
+            uploader.destroy();
+        }
+
+        function onUploadProgress(uploader, file) {
+            deferred.notify(file);
+        }
+
+        function onError (uploader, err) {
+            deferred.reject(err);
+        }
+
+        uploader.start();
+        return deferred.promise;
+    }
+
+    /**
+     * Iterates over a list, applying a function to each element.
+     * The function must be an asynchronous effect. The results are accumulated and returned.
+     */
+    function asyncEachSerial (fAsyncEffect, list) {
+
+        var acc = [];
+
+        var iterate = function (next, rest) {
+            var promise = fAsyncEffect(next);
+            return promise.fin(function (response) {
+                acc.push(response);
+                return ((list.length === 0) ? acc : iterate(_.head(rest), _.tail(rest)));
+            });
+        }
+
+        return iterate(_.head(list), _.tail(list));
+    }
+
+
+
 
     return FileUploader;
 });
